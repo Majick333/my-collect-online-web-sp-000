fd4474bea81c8c04f71317652e484bbf178093d8
Ryan M <rmccauleymusic@gmail.com>
2019-10-26 20:29:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
 
 def my_collect(array)
   i = 0
-  collection = []
-  my_collect(array) do |item|
-    while i < yield(array.length)
-    collection << yield(array[i])
+  new_collection = []
+  #my_collect(array) do |item|
+    while i < array.length
+    new_collection.push << yield(array[i])
     i = i + 1
 end
+new_collection
 end
